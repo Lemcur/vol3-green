9ffe8a0d9af37621095184b080add93c52b31480
lemcur <loernittcc@gmail.com>
2018-10-05 20:32:17 +0200

    caterings should be able to see caterings list (their own for example)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :devise_parameter_sanitizer, if: :devise_controller?
 
+  private
+  def authenticate!
+    if [current_user, current_catering].all? { |i| i.nil? }
+      authenticate_user!
+    end
+  end
+
   protected
 
   def devise_parameter_sanitizer
--- app/controllers/caterings_controller.rb
+++ app/controllers/caterings_controller.rb
@@ -1,5 +1,5 @@
 class CateringsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate!
 	def index
     @caterings = Catering.all
 	end
